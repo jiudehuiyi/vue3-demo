f67473b8a1e940a547a72a8298320e26b745c959
jiudehuiyi <136859304@qq.com>
2024-07-03 14:44:42 +0800

    auto deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -x  # 这里是为了看错误日志
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'auto deploy'
+
+
+git push -f https://github.com/jiudehuiyi/vue3-demo.git main:static-pages
